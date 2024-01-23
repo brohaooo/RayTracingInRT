f8d07fdf8e29b6fe5adeb58e454f10afb3a7662d
Lin Xinhao <brohaooo@gmail.com>
2024-01-22 19:41:17 -0500

    glClearColor

--- include/RT_renderer.h
+++ include/RT_renderer.h
@@ -105,6 +105,8 @@ class RT_renderer {
         glEnable(GL_BLEND);
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
 
 
 
@@ -206,7 +208,6 @@ class RT_renderer {
         // render part is here
         // ------
         // clear screen
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 
