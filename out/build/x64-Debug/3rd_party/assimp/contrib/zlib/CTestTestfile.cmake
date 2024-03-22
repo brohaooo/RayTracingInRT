0f0833f1acba0d899539287cdc82d517b0b64acc
Lin Xinhao <brohaooo@gmail.com>
2024-03-22 16:47:17 -0400

    x

--- out/build/x64-Debug/3rd_party/assimp/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/86189/Desktop/RayTracing/RayTracingInRT/3rd_party/assimp/contrib/zlib
-# Build directory: C:/Users/86189/Desktop/RayTracing/RayTracingInRT/out/build/x64-Debug/3rd_party/assimp/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
